961508de88475e41820bd726992fe07ed5ca50d4
swayammaheshwari <swayammaheshwari890@gmail.com>
2022-11-28 00:01:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGOLAB_URI, {useNewUrlParser: true});
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+
+
+
+
+
+
+app.listen(process.env.PORT, function() {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
+
+
+
+//PORT=5000
+//MONGOLAB_URI=mongodb://localhost/
